65ae84c63a6eec49f40ed5aa5976f4535be5e85f
Prajwal518 <prajwalgowda.s518@gmail.com>
2021-01-25 11:07:17 +0530

    added hamburger menu animation with a background for the menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+const hamburger = document.querySelector('#menubtn');
+const header = document.querySelector('.header');
+
+hamburger.addEventListener('click', function(){
+    if(header.classList.contains('open')){
+        header.classList.remove('open')
+    }else{
+        header.classList.add('open');
+    }
+});
