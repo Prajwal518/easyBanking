e4c228f68319996b84f264ffee1ac0a83342611d
Prajwal518 <prajwalgowda.s518@gmail.com>
2021-01-25 20:34:27 +0530

    add the complete animation of hamburger menu and added link and animations to it. with this nav bar for mobile and desktop is complete

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,24 @@
 const hamburger = document.querySelector('#menubtn');
+const noscroll = document.querySelector('body');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade')
 
-hamburger.addEventListener('click', function(){
+hamburger.addEventListener('click', function(){//close menu hamburger
     if(header.classList.contains('open')){
-        header.classList.remove('open')
-    }else{
+        noscroll.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElements.forEach((element) => {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+        
+    }else{//opne menu hamburger
+        noscroll.classList.add('noscroll');
         header.classList.add('open');
+        fadeElements.forEach((element) => {
+                element.classList.remove('fade-out');
+                element.classList.add('fade-in');
+            });
     }
 });
